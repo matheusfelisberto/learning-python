2b70a9c397d12c53243964dafbbe1892c36b2f0c
Matheus Felisberto <matheus.felisberto@hotmail.com>
2018-01-10 01:19:27 -0200

    add(lib) numpy

---
+++ Numpy.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy
+
+
+# In[3]:
+
+
+numbers = [1, 4, 5, 9, 10, 45]
+
+
+# In[4]:
+
+
+numpy.mean(numbers)
+
+
+# In[5]:
+
+
+numpy.median(numbers)
+
+
+# In[6]:
+
+
+numpy.std(numbers)
+
